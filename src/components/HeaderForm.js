cb7f9a257495f369ed25f2c40967556dbad41199
MBaroky <m.baroky@gmail.com>
2024-08-21 01:54:02 +0300

    welcom section done

--- src/components/HeaderForm.js
+++ src/components/HeaderForm.js
@@ -1,7 +1,6 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import arrowIcon from "../assets/arrow-icon.svg";
 
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
@@ -33,12 +32,15 @@ export default function HeaderForm({ children }) {
       );
   };
   return (
-    <div className="flex min-w-[300px] basis-1/3 flex-col bg-white px-8 py-11 text-base font-light max-md:max-w-full max-md:px-5 max-md:py-1">
-      <div className="flex w-full justify-between gap-5 text-4xl font-bold max-md:max-w-full max-md:flex-wrap">
-        <h1 className="text-black">
-          Book A <span className="font-outline-1 text-white ">Demo</span>
-        </h1>
+    <div className="relative z-10 flex w-full min-w-[300px] basis-1/3 flex-col items-center justify-center px-8 py-11 text-base font-light max-md:max-w-full max-md:px-5 max-md:py-1">
+      <div className="flex w-full justify-center text-2xl font-semibold max-md:max-w-full max-md:flex-wrap">
+        <h1 className="text-black">Get A Free SEO Audit</h1>
       </div>
+      <hr class="yellow-bg my-2 h-1 w-[80px] border-t-0 dark:bg-white/10" />
+      <p className="mt-3 w-[300px] text-center text-black">
+        Please share the details below to get started with our services.
+      </p>
+
       {formSuccess ? (
         <h2 className="text-l mt-5 text-center text-black">
           Thank you for contacting us! <br />
@@ -49,7 +51,7 @@ export default function HeaderForm({ children }) {
           <form
             ref={form}
             noValidate={true}
-            className="group mt-5 max-md:mt-1"
+            className="group mt-5 flex w-full flex-col max-md:mt-1"
             onSubmit={handleSubmit}
           >
             <Input
@@ -67,7 +69,7 @@ export default function HeaderForm({ children }) {
 
             <Input
               type="email"
-              placeholder="Business Email"
+              placeholder="Email Address"
               id="email"
               className="peer mt-1 rounded-none border-l-0 border-r-0 border-t-0 border-black text-black md:mt-3 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
               pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
@@ -107,15 +109,9 @@ export default function HeaderForm({ children }) {
 
             <Button
               type="submit"
-              className="mt-12 flex gap-2 self-start whitespace-nowrap rounded-none bg-violet-700 px-5 py-3 text-base font-medium text-white hover:bg-white hover:text-black group-invalid:pointer-events-none max-md:mt-10  max-md:px-5"
+              className="mt-12 flex gap-2 self-center whitespace-nowrap rounded-full border border-solid border-black bg-white px-5 py-3 text-base font-medium text-black hover:bg-black  hover:text-white group-invalid:pointer-events-none max-md:mt-10 max-md:px-5"
             >
-              <span>Submit</span>
-              <img
-                loading="lazy"
-                src={arrowIcon}
-                alt=""
-                className="aspect-[1.49] w-[27px] shrink-0 self-start"
-              />
+              <span>Let’s Get Started</span>
             </Button>
           </form>
 
